65e2247dcdfe2ab1340a558cba298e5001b8178d
oswaldo <vnkvp@outlook.com>
2020-09-22 14:20:20 -0300

    hardcoded home styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 import NearMeOutlinedIcon from '@material-ui/icons/NearMeOutlined';
 import ExploreOutlinedIcon from '@material-ui/icons/ExploreOutlined';
 import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded';
+import SearchIcon from '@material-ui/icons/Search';
 import '../assets/Header.css'
 
 function Header() {
@@ -12,7 +13,10 @@ function Header() {
             <img
                 src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F2a%2FInstagram_logo.svg%2F1280px-Instagram_logo.svg.png&f=1&nofb=1'
                 alt='logo' />
-            <input type='text' placeholder='Search' />
+            <div className='header__input'>
+                <SearchIcon className='header__searchicon'/>
+                <input type='text' placeholder='Search' />
+            </div>
             <div className='header__links'>
                 <HomeRoundedIcon />
                 <NearMeOutlinedIcon />
--- src/components/Post.js
+++ src/components/Post.js
@@ -6,11 +6,11 @@ function Post() {
     return (
         <div className='post'>
             <div className='post__header'>
-            <Avatar alt='Oswaldo Inacio'
-            className='post__avatar'
-            src='/static/images/avatar/1.jpg'
-            />
-            <h3>oswaldoinaciod</h3>
+                <Avatar alt='Oswaldo Inacio'
+                    className='post__avatar'
+                    src='/static/images/avatar/1.jpg'
+                />
+                <h3>oswaldoinaciod</h3>
             </div>
             {/* header= avatar username */}
             <img src='https://miro.medium.com/max/1200/1*y6C4nSvy2Woe0m7bWEn4BA.png' alt='' className='post__image' />
